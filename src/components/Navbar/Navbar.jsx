2420339482ac2ec5bbfa403f0b8288ab3d992b59
Farhan Yousaf <farhanyousaf1996@gmail.com>
2023-01-10 13:08:36 -0500

    added hovers to login link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,16 +7,23 @@ import './Navbar.css';
 const Navbar = () => (
   <nav className='app__navbar'>
     <div className='app__navbar-logo'>
-    <img src={images.gericht} alt="app logo">
-    </img>
+      <img src={images.gericht} alt="app logo">
+      </img>
+
     </div>
     <ul className='app__navbar-links'>
-    <li className='p__opensans'><a href='#home'>Home</a></li>
-    <li className='p__opensans'><a href='#about'>About</a></li>
-    <li className='p__opensans'><a href='#menu'>Menu</a></li>
-    <li className='p__opensans'><a href='#awards'>Awards</a></li>
-    <li className='p__opensans'><a href='#contact'>Contact</a></li>
+      <li className='p__opensans'><a href='#home'>Home</a></li>
+      <li className='p__opensans'><a href='#about'>About</a></li>
+      <li className='p__opensans'><a href='#menu'>Menu</a></li>
+      <li className='p__opensans'><a href='#awards'>Awards</a></li>
+      <li className='p__opensans'><a href='#contact'>Contact</a></li>
     </ul>
+    
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">Log In / Registration</a>
+      <div />
+      <a href="/" className="p__opensans">Book Table</a>
+    </div>
   </nav>
 );
 
