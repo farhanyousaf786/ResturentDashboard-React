1cdd9e2132f8ce9a72b7484f4cd602e8a20e53d6
Farhan Yousaf <farhanyousaf1996@gmail.com>
2023-01-10 13:02:21 -0500

    added componenet in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,19 @@ import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div className='app__navbar-logo'>
+    <img src={images.gericht} alt="app logo">
+    </img>
+    </div>
+    <ul className='app__navbar-links'>
+    <li className='p__opensans'><a href='#home'>Home</a></li>
+    <li className='p__opensans'><a href='#about'>About</a></li>
+    <li className='p__opensans'><a href='#menu'>Menu</a></li>
+    <li className='p__opensans'><a href='#awards'>Awards</a></li>
+    <li className='p__opensans'><a href='#contact'>Contact</a></li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
