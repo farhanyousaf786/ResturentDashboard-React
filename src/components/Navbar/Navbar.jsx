ca05396c66e7b2147ddceeb57fb7fb3c6fb1f30e
Farhan Yousaf <farhanyousaf1996@gmail.com>
2023-01-10 13:45:28 -0500

    added media query and other things on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,6 @@ const Navbar = () => (
     <div className='app__navbar-logo'>
       <img src={images.gericht} alt="app logo">
       </img>
-
     </div>
     <ul className='app__navbar-links'>
       <li className='p__opensans'><a href='#home'>Home</a></li>
@@ -18,12 +17,25 @@ const Navbar = () => (
       <li className='p__opensans'><a href='#awards'>Awards</a></li>
       <li className='p__opensans'><a href='#contact'>Contact</a></li>
     </ul>
-    
     <div className="app__navbar-login">
       <a href="#login" className="p__opensans">Log In / Registration</a>
       <div />
       <a href="/" className="p__opensans">Book Table</a>
     </div>
+    <div className='app__navbar-smallscreen_overlay flex__center slide-bttom'>
+      <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => { }} />
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => { }} />
+      <ul className='app__navbar-smallscreen-links'>
+        <li className='p__opensans'><a href='#home'>Home</a></li>
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#contact'>Contact</a></li>
+      </ul>
+    </div>
+
   </nav>
 );
 
